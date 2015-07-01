e02d37e54f69e8cba70d94a12b7e67db4f998e6d
Arthur Xavier <arthur.xavierx@gmail.com>
2015-07-01 14:56:29 +0200

    UserProfile component created for better modularization.

---
+++ src/main/java/com/croak/croak/components/UserProfile.java
@@ -0,0 +1,48 @@
+package com.croak.croak.components;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.tapestry5.*;
+import org.apache.tapestry5.annotations.*;
+import org.apache.tapestry5.ioc.annotations.*;
+import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.SymbolConstants;
+
+import com.croak.croak.entities.User;
+import com.croak.croak.rest.UserResource;
+import com.croak.croak.rest.CroakResource;
+
+public class UserProfile {
+
+  @Inject
+  private UserResource userResource;
+  @Inject
+  private CroakResource croakResource;
+
+  @Parameter(required = true)
+  @Property
+  private User user;
+
+  @Property
+  private int followersNumber;
+  @Property
+  private int followingNumber;
+  @Property
+  private int croaksNumber;
+
+  @BeginRender
+  public void beginRender() {
+    this.followingNumber = this.user.getSubscriptions().size();
+
+    Set followers = new HashSet<User>();
+    for(User u : userResource.getUsers()) {
+      if(u.getSubscriptions().contains(this.user)) {
+        followers.add(u);
+      }
+    }
+    this.followersNumber = followers.size();
+
+    this.croaksNumber = croakResource.getCroaksByUser(this.user.getUsername()).size();
+  }
+}
--- src/main/java/com/croak/croak/pages/Home.java
+++ src/main/java/com/croak/croak/pages/Home.java
@@ -1,46 +1,21 @@
 package com.croak.croak.pages;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.apache.tapestry5.*;
 import org.apache.tapestry5.annotations.*;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 import com.croak.croak.entities.User;
 import com.croak.croak.rest.UserResource;
-import com.croak.croak.rest.CroakResource;
 
 public class Home {
 
   @Inject
   private UserResource userResource;
-  @Inject
-  private CroakResource croakResource;
 
   @Property
   private User user;
 
-  @Property
-  private int followersNumber;
-  @Property
-  private int followingNumber;
-  @Property
-  private int croaksNumber;
-
   public void onActivate() {
     this.user = userResource.getUser(0L);
-
-    this.followingNumber = this.user.getSubscriptions().size();
-
-    Set followers = new HashSet<User>();
-    for(User u : userResource.getUsers()) {
-      if(u.getSubscriptions().contains(this.user)) {
-        followers.add(u);
-      }
-    }
-    this.followersNumber = followers.size();
-
-    this.croaksNumber = croakResource.getCroaksByUser(this.user.getUsername()).size();
   }
 }
